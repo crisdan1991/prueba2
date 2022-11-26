e45411232082b164616f735d6165712d14d08b77
Jhony Llano <cristiandan_1991@hotmail.com>
2022-11-26 13:01:47 -0500

    test1

--- vista/src/app/app.component.spec.ts
+++ vista/src/app/app.component.spec.ts
@@ -1,7 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+@Component({
+  selector: 'app-banner',
+  template: '<h1>{{title}}</h1>',
+  styles: ['h1 { color: green; font-size: 350%}']
+})
+export class BannerComponent {
+  title = 'DEMOSTRACION LLano - Velasco v2';
+}
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
