eba3059f0dc63905410564f1c6af0e54914df6df
Jhony Llano <cristiandan_1991@hotmail.com>
2022-11-28 16:47:12 -0500

    a

--- vista/src/app/Service/service.service.spec.ts
+++ vista/src/app/Service/service.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClient, HttpHandler} from '@angular/common/http';
 import { ServiceService } from './service.service';
 
 describe('ServiceService', () => {
+
   let service: ServiceService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    TestBed.configureTestingModule({providers:[HttpHandler,HttpClient]});
     service = TestBed.inject(ServiceService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  it('should get serice', () => {
+    service = TestBed.inject(ServiceService);
+    const result = service.getPersonas();
+    expect(result).toBeTruthy();
+    let respuesta;
+    service.getPersonas().subscribe(res=> {
+      respuesta = res
+      }, (error) => {
+      console.log(error);
+      });
+    console.log(respuesta);
   });
+
 });
